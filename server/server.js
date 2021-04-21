53dd4e04f07394b64584a4c624a44882745401e2
Doug Riblet <smashzen85@gmail.com>
2021-04-21 13:45:58 -0500

    remove apollo server files, split off into new repo

--- server/server.js
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from 'apollo-server';
-import typeDefs from './typeDefs.js';
-import resolvers from './resolvers.js';
-
-const port = process.env.PORT || 8246;
-
-new ApolloServer({ resolvers, typeDefs }).listen({ port }, () =>
-  console.log(`Server ready at: http://localhost:${port}`),
-);
